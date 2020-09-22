13a7b8c1dc48683acefbed54bf84ae59122098aa
Ben O'Neill <47801098+ben-oneill@users.noreply.github.com>
2020-09-22 12:36:54 +1000

    Update plot.spectrum.test.R

--- R/plot.spectrum.test.R
+++ R/plot.spectrum.test.R
@@ -6,7 +6,7 @@
 #' The plots also report the value of the maximum scaled intensity and the resulting p-value for the test.  This dual plot forms a useful
 #' companion to the permutation-spectrum test; it allows the user to visualise the simulated null distribution and test statistic.
 #'
-#' @param test A ```spectrum.test``` object produced by the ```spectrum.test``` function
+#' @param x A ```spectrum.test``` object produced by the ```spectrum.test``` function
 #' @param ggplot Logical; if ```TRUE``` the scatterplot is a ```ggplot``` object; if ```FALSE``` it is a ```base``` plot object
 #' @param print Logical; if ```TRUE``` the scatterplot is printed
 #' @param ...   unused
@@ -22,10 +22,10 @@
 plot.spectrum.test <- function(test, ggplot = TRUE, print = TRUE, ...) {
 
   #Check test input
-  if (is.null(test[["maxint.sim"]])) { stop('Error: test object should contain simulated intensity values in maxint.sim') } else {
-    maxint.sim <- test[["maxint.sim"]] }
-  if (is.null(test[["x"]]))          { stop('Error: test object should contain a time-series vector x') } else {
-    x <- test[["x"]] }
+  if (is.null(x[["maxint.sim"]])) { stop('Error: test object should contain simulated intensity values in maxint.sim') } else {
+    maxint.sim <- x[["maxint.sim"]] }
+  if (is.null(x[["x"]]))          { stop('Error: test object should contain a time-series vector x') } else {
+    x <- x[["x"]] }
 
   #Check other inputs
   if (!is.numeric(x)) {
