3b5334c8b30721200739e53883f223c15169226b
Neal Fultz <nfultz@gmail.com>
2023-04-29 22:18:52 -0700

    bump version for CRAN, fix some documentation warnings

--- R/filter.intensity.R
+++ R/filter.intensity.R
@@ -1,17 +1,18 @@
 #' Filter a time-series vector/matrix based on its spectral intensity
-#' 
+#'
 #' \code{filter.intensity} returns a vector/matrix of filtered time-series
 #'
-#' This function filters a time-series based on its spectral intensity.  The user inputs the time-series vector or matrix and 
-#' specifies whether it is to be centered and/or scaled.  Centering subtracts the sample mean of the vector prior to conversion 
-#' into frequency space; this sets the intensity of the signal to zero at the zero frequency.  Scaling scales the intensity so 
-#' the that norm of the intensity vector is equal to the number of values in the time-series.  The function then applies the 
+#' This function filters a time-series based on its spectral intensity.  The user inputs the time-series vector or matrix and
+#' specifies whether it is to be centered and/or scaled.  Centering subtracts the sample mean of the vector prior to conversion
+#' into frequency space; this sets the intensity of the signal to zero at the zero frequency.  Scaling scales the intensity so
+#' the that norm of the intensity vector is equal to the number of values in the time-series.  The function then applies the
 #' specified highpass/lowpass filter and returns the filtered signals.  By default, both the highpass and lowpass filters remove
-#' parts of the signal that do not meet the filter requirements; if preferred the filters can attenuate (rather than removing) 
+#' parts of the signal that do not meet the filter requirements; if preferred the filters can attenuate (rather than removing)
 #' these signals.
 #'
-#' @usage \code{filter.intensity(x, centered = TRUE, centred = centered, scaled = TRUE, 
-#'                               highpass = NULL, highpass.remove = TRUE, lowpass  = NULL, lowpass.remove  = TRUE)}
+#' @usage filter.intensity(x, centered = TRUE, centred = centered, scaled = TRUE,
+#'          highpass = NULL, highpass.remove = TRUE,
+#'          lowpass  = NULL, lowpass.remove  = TRUE)
 #' @param x A vector of time-series values
 #' @param centered Logical; if \code{TRUE} the time-series vector is centred (mean removed) before computing its intensity
 #' @param centred An alternative name for the \code{centered} input
@@ -21,8 +22,8 @@
 #' @param lowpass The maximum intensity value for the lowpass filter (or NULL if there is no lowpass filter)
 #' @param lowpass.remove Logical; if \code{TRUE} then the lowpass filter removes (instead of attenuating) the high-frequency signal
 
-filter.intensity <- function(x, centered = TRUE, centred = centered, scaled = TRUE, 
-                             highpass = NULL, highpass.remove = TRUE, 
+filter.intensity <- function(x, centered = TRUE, centred = centered, scaled = TRUE,
+                             highpass = NULL, highpass.remove = TRUE,
                              lowpass  = NULL, lowpass.remove  = TRUE) {
 
   #Check input x
@@ -36,13 +37,13 @@ filter.intensity <- function(x, centered = TRUE, centred = centered, scaled = TR
   if (m == 0)        { stop('Error: x must have at least one value') }
   if (m == 1)        {
     if (scaled)                { stop('Error: x must have at least two values for scaled intensity') } }
-  
+
   #Check inputs centred/centered and scaled
   if (!is.logical(centred))  { stop('Error: centered/centred must be a logical value') }
   if (length(centred) != 1)  { stop('Error: centered/centred must be a single logical value') }
   if (!is.logical(scaled))   { stop('Error: scaled must be a logical value') }
   if (length(scaled) != 1)   { stop('Error: scaled must be a single logical value') }
-  
+
   #Check inputs highpass and lowpass
   if (!is.null(highpass)) {
     if (!is.numeric(highpass)) stop('Error: highpass must be a positive number (or NULL)')
@@ -57,7 +58,7 @@ filter.intensity <- function(x, centered = TRUE, centred = centered, scaled = TR
   if ((is.null(highpass))&(is.null(lowpass))) {
     warning('You have not specified highpass or lowpass --- no filter applied')
     return(x) }
- 
+
   #Compute DFT and intensity
   DFT <- matrix(complex(1), nrow = n, ncol = m)
   rownames(DFT) <- rownames(x)
@@ -72,27 +73,27 @@ filter.intensity <- function(x, centered = TRUE, centred = centered, scaled = TR
     SD[i]   <- sqrt(sum((Mod(x[i,] - MEAN[i])^2)/(m-1)))
     DFT[i,] <- fft(x[i,] - centred*MEAN[i])/sqrt(m)
     INT[i,] <- Mod(DFT[i,])
-    if (centred) { 
+    if (centred) {
       DFT[i, 1] <- 0
-      INT[i, 1] <- 0 } else { 
+      INT[i, 1] <- 0 } else {
       SD[i] <- sqrt((m-1)/m)*SD[i] }
-    if (scaled)  { 
+    if (scaled)  {
       DFT[i,] <- DFT[i,]/SD[i]
       INT[i,] <- INT[i,]/SD[i] } }
-  
+
   #Filter the signal using highpass/lowpass filter
   DFT.FILT <- DFT
   INT.FILT <- INT
   x.FILT   <- matrix(is.complex(0), nrow = n, ncol = m)
   for (i in 1:n) {
-    if (!is.null(lowpass))  { 
+    if (!is.null(lowpass))  {
       for (k in 1:m) {
-        if (INT[i, k] > lowpass)  { 
+        if (INT[i, k] > lowpass)  {
           DFT.FILT[i, k] <- ifelse(lowpass.remove,  0, (DFT[i, k]/INT[i, k])*lowpass)
           INT.FILT[i, k] <- Mod(DFT.FILT[i, k]) } } }
-    if (!is.null(highpass)) { 
+    if (!is.null(highpass)) {
       for (k in 1:m) {
-        if (INT[i, k] < highpass) { 
+        if (INT[i, k] < highpass) {
           DFT.FILT[i, k] <- ifelse(highpass.remove, 0, (DFT[i, k]/INT[i, k])*highpass)
           INT.FILT[i, k] <- Mod(DFT.FILT[i, k]) } } }
     x.FILT[i, ]   <- fft(DFT.FILT[i, ], inverse = TRUE)/sqrt(m)
